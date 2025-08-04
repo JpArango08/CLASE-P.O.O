7b6abc37f3e305fee627a34d0f8580c6826a7be5
Your Name <you@example.com>
2025-08-03 19:29:20 -0500

    Tarea

--- EJERCICIOS.py
+++ EJERCICIOS.py
@@ -1,7 +1,7 @@
 # Estás desarrollando un programa para llevar el control académico en una institución educativa. 
 # Se debe poder registrar el nombre, la edad y las 3 calificaciones de los estudiantes (Supongamos que solo hay 3 materias).
 # Además, el sistema debe permitir calcular el promedio de cada uno y mostrar sus datos personales junto con el promedio.
-"
+
 class Calificaciones:
     def __init__(self, Nombre, edad, M1, M2, M3):
         self.Nombre=Nombre
@@ -32,7 +32,12 @@ M3=float(input())
 
 estudiante=Calificaciones(Nombre,edad,M1,M2,M3)
 promEstudiante= estudiante.Prom()
-print("El promedio de", estudiante.Nombre,"es", promEstudiante)"
+print("El promedio de", estudiante.Nombre,"es", promEstudiante)
+
+# Una tienda quiere llevar el control de los productos que vende. 
+# Por cada producto, necesita guardar el nombre, el precio y la cantidad disponible. 
+# El sistema debe permitir vender cierta cantidad de productos y mostrar cuántas unidades quedan. 
+# Si no hay suficientes unidades, debe mostrar un mensaje de advertencia.
 
 
 
